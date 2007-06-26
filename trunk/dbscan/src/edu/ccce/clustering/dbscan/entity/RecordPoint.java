5fee134f5adeabcf878332e39b9a1727064a2b7c
lixfcn <lixfcn@a647e5aa-bd33-0410-bf15-d1b47774bbb0>
2007-06-26 04:19:39 +0000

    初始导入。

---
+++ trunk/dbscan/src/edu/ccce/clustering/dbscan/entity/RecordPoint.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package edu.ccce.clustering.dbscan.entity;
+
+/**
+ * @author Tony 2007-6-25
+ */
+public class RecordPoint {
+	String ID = "0";
+	int x = 0;
+	int y = 0;
+	short r = 0;
+	short g = 0;
+	short b = 0;
+	int cls = -1;	
+	
+	
+	
+	/**
+	 * @param from	the point from
+	 * @param to   the point to
+	 * @return	the distance entity
+	 */
+	public static double calculateDiscance(RecordPoint from,RecordPoint to,double width,double height){
+		
+		
+		double pointdistance = -1;
+		
+		if(from != null && to != null){
+			double distance_x = Math.pow((from.x - to.x)/width,2);	// this may calcalate a standarded diff.
+			double distance_y = Math.pow((from.y - to.y)/height,2);
+			double distance_r = Math.pow((from.r - to.r)/255.0,2);
+			double distance_g = Math.pow((from.g - to.g)/255.0,2);
+			double distance_b = Math.pow((from.b - to.b)/255.0,2);
+			double distance = Math.sqrt(distance_x + distance_y + distance_r + distance_g + distance_b);
+			pointdistance = distance;
+		}		
+		return pointdistance;		
+	}
+
+
+
+	/**
+	 * @param id
+	 * @param x
+	 * @param y
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @param cls
+	 */
+	protected RecordPoint(String id, int x, int y, short r, short g, short b) {
+		super();
+		ID = id;
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		cls = -1;
+	}
+
+
+
+	/**
+	 * @param id
+	 * @param r
+	 */
+	protected RecordPoint() {
+		super();
+		cls = -1;
+	}
+}
---
+++ trunk/dbscan/src/edu/ccce/clustering/dbscan/main/mainentry.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.ccce.clustering.dbscan.main;
+
+/**
+ * @author Tony 2007-6-25
+ */
+public class mainentry {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
